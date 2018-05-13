643a984a0852d92a90a3e2390bcd04171652e0df
andrew shaw <andrewoutrammshaw@mail.com>
2018-05-13 15:37:45 +0100

    added questions and forms

---
+++ FastGoat/Controllers/QuestionController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FastGoat.Models;
+
+namespace FastGoat.Controllers
+{
+    public class QuestionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Questions()
+        {
+            List<Question> QuestionList1 = new List<Question>
+                            {
+                                new Question{ QuestionText = "QG1 Q1"},
+                                new Question{ QuestionText = "QG1 Q2"}
+                            };
+            List<Question> QuestionList2 = new List<Question>
+                            {
+                                new Question{ QuestionText = "QG2 Q1"},
+                                new Question{ QuestionText = "QG2 Q2"},
+                                new Question{ QuestionText = "QG2 Q3"},
+                                new Question{ QuestionText = "QG2 Q4"}
+                            };
+            List<Question> QuestionList3 = new List<Question>
+                            {
+                                new Question{ QuestionText = "QG3 Q1"},
+                                new Question{ QuestionText = "QG3 Q2"},
+                                new Question{ QuestionText = "QG3 Q3"}
+                            };
+
+            List<QuestionGroup> Group1 = new List<QuestionGroup>
+                    {
+                        new QuestionGroup
+                        {
+                            Questions = QuestionList1,
+                            GroupTitle = "Lashunta"
+                        },
+                        new QuestionGroup
+                        {
+                            Questions = QuestionList2,
+                            GroupTitle = "Shirren"
+                        },new QuestionGroup
+                        {
+                            Questions = QuestionList3,
+                            GroupTitle = "Vesk"
+                        }
+                    };
+            Questionnaire questionnaire = new Questionnaire
+            {
+                QuestionGroups = Group1
+            };
+            return View(questionnaire);
+        }
+        [HttpPost]
+        public IActionResult Questions(Questionnaire questionnaire)
+        {
+            return Redirect("../Home");
+        }
+    }
+}
---
+++ FastGoat/Models/Questionnaire.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastGoat.Models
+{
+    public class Questionnaire
+    {
+        public IList<QuestionGroup> QuestionGroups
+        {
+            get;
+            set;
+        }
+    }
+
+    public class QuestionGroup
+    {
+        public IList<Question> Questions
+        {
+            get;
+            set;
+        }
+        public String GroupTitle { get; set; }
+    }
+
+    public class Question
+    {
+        public String QuestionText { get; set; }
+        public String QuestionAnswer { get; set; }
+    }
+}
